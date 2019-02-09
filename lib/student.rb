cfc838fcf4a2dacf278f9f411fcdb1dd08d312a3
Kathleen Carrel <katiecarrel@gmail.com>
2019-02-09 21:37:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,9 +55,9 @@ class Student
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE student = ?"
     
-    DB[:conn].execute(sql,name).map do |row|
-      self.new_from_db(row)
-    end.first
+    result = DB[:conn].execute(sql,name)[0] 
+    Student.new(result[0],result[1],result[2])
+    end
   end 
   
   def update 
